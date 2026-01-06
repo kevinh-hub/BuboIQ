2cd845f2280d61f8520f3269582ad91f61d18734
kevinh-hub <kevinh@buboiq.com>
2026-01-06 15:40:53 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,13 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
-    emptyOutDir: true,
-    sourcemap: false
+    rollupOptions: {
+      // This tells the builder to ignore missing files and keep going
+      onwarn(warning, warn) {
+        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return;
+        if (warning.code === 'THIS_IS_UNDEFINED') return;
+        warn(warning);
+      },
+    },
   }
 });
